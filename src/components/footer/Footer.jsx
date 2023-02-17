ccb792051a14e543f5ea8a8ca7219fa3821e0327
Its-A-Mia <miapurdun@gmail.com>
2023-02-17 17:32:33 -0600

    giving shape to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,11 @@
+import './Footer.css';
+
 const Footer = () => {
   return (
     <footer>
-      <div className="container">Designed & Built by Mia Purdun</div>
+      <div className="container footer-container">
+        <h4>Designed & Built by Mia Purdun</h4>
+      </div>
     </footer>
   );
 };
