071c6e7ef8aa82df835cd20aaee15079b8208735
Its-A-Mia <miapurdun@gmail.com>
2023-02-17 02:35:32 -0600

    setting up base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
 import './styles/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <Header />
+        <Footer />
+      </main>
+    </div>
+  );
 }
 
 export default App;
