5c8df53fa0cb4b0407f915b5ce1344fd34e6bb5c
Its-A-Mia <miapurdun@gmail.com>
2023-02-19 21:54:25 -0600

    swapped color palette inputs with custom properties

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,11 +5,13 @@ import ProjectsNav from './ProjectsNav';
 const Projects = () => {
   return (
     <section className="projects">
+      <div className="golden-border-top"></div>
       <div className="container grid">
         <h2 className="section-title projects-title">Projects</h2>
         <ProjectsNav />
         <ProjectsCard />
       </div>
+      <div className="golden-border-bottom"></div>
     </section>
   );
 };
