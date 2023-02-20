35003247ec671acb30b26fb34adf19ba02e9143b
Its-A-Mia <miapurdun@gmail.com>
2023-02-19 20:42:23 -0600

    svg, adjusted button style home

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,25 @@
 import './Contact.css';
 import ContactInput from './ContactInput';
+import sendMessageIcon from '../../assets/send-message-icon.svg';
+import SendMessageIcon from './SendMessageIcon';
 
 const Contact = () => {
   return (
-    <section className="container grid">
-      <ContactInput label={'Name'} type={'text'} />
-      <ContactInput label={'Email'} type={'text'} />
-      <ContactInput label={'Message'} type={'textarea'} />
+    <section className="container  grid">
+      <h2 className="section-title contact-title">Contact Me</h2>
+
+      <div className="contact-input-container">
+        <form>
+          <ContactInput label={'Name'} type={'text'} />
+          <ContactInput label={'Email'} type={'text'} />
+          <ContactInput label={'Message'} type={'textarea'} />
+          <button className="contact-input-submit">Send Message</button>
+        </form>
+      </div>
+
+      <div className="contact-icon-container">
+        <SendMessageIcon />
+      </div>
     </section>
   );
 };
