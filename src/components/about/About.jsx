206850ba9acbe5621e08cab73aba20b773d78313
Its-A-Mia <miapurdun@gmail.com>
2023-02-19 23:25:43 -0600

    added section scrolling by using nav bar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import AboutBio from './AboutBio';
 
 const About = () => {
   return (
-    <section>
+    <section id="about">
       <div className="container grid">
         <h2 className="section-title about-title">Journey and Skills</h2>
         <AboutBio />
