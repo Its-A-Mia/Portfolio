ebf617bb1dcdcec521d052a3d32f7a2707fa5e52
Its-A-Mia <miapurdun@gmail.com>
2023-02-21 13:01:49 -0600

    added contact feature

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import SendMessageIcon from '../icons/SendMessageIcon';
 import './Contact.css';
 import ContactInput from './ContactInput';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
   return (
     <section className="container grid super-section" id="contact">
       <h2 className="section-title contact-title">Contact Me</h2>
 
       <div className="contact-input-container">
-        <form>
-          <ContactInput label={'Name'} type={'text'} />
-          <ContactInput label={'Email'} type={'email'} />
-          <ContactInput label={'Message'} type={'textarea'} />
+        <form action="https://formsubmit.co/miapurdun@gmail.com" method="POST">
+          {/* formsubmit.co api */}
+          <input
+            type="hidden"
+            name="_autoresponse"
+            value="Thank you for reaching out! I will respond to you soon."
+          ></input>
+          <input type="hidden" name="_template" value="table"></input>
+          {/* formsubmit.co api */}
+          <ContactInput label={'Name'} type={'text'} name={'name'} value={name} setValue={setName} />
+          <ContactInput label={'Email'} type={'email'} name={'email'} value={email} setValue={setEmail} />
+          <ContactInput label={'Message'} type={'textarea'} value={message} name={'message'} setValue={setMessage} />
           <button className="contact-input-submit">Send Message</button>
         </form>
       </div>
--- src/components/contact/ContactInput.jsx
+++ src/components/contact/ContactInput.jsx
@@ -1,4 +1,4 @@
-const ContactInput = ({ label, type, value, setValue }) => {
+const ContactInput = ({ label, type, value, setValue, name }) => {
   return (
     <label className="contact-input-label">
       {label}
@@ -6,11 +6,20 @@ const ContactInput = ({ label, type, value, setValue }) => {
         <textarea
           className="contact-input"
           value={value}
+          name={name}
           onChange={(e) => setValue(e.target.value)}
           style={{ resize: 'vertical' }}
+          required
         ></textarea>
       ) : (
-        <input className="contact-input" type={type} value={value} onChange={(e) => setValue(e.target.value)} />
+        <input
+          className="contact-input"
+          type={type}
+          name={name}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
       )}
     </label>
   );
