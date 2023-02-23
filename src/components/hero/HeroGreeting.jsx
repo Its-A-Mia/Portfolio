3b7718bfb45f0f6691a8c59ef590b0695ce3ad93
Its-A-Mia <miapurdun@gmail.com>
2023-02-23 00:20:48 -0600

    added some cool animations to hero section. Thanks for the suggestions, Wesley!! :)

--- src/components/hero/HeroGreeting.jsx
+++ src/components/hero/HeroGreeting.jsx
@@ -1,9 +1,9 @@
 const HeroGreeting = () => {
   return (
     <div className="hero-greeting-container">
-      <h1>Hi, I'm Mia</h1>
-      <h2 className="hero-subtitle">Budding front-end developer</h2>
-      <h3 className="subtitle">I design and code beautiful websites</h3>
+      <h1 className="hero-greeting-title">Hi, I'm Mia</h1>
+      <h2 className="hero-greeting-subtitle">Budding front-end developer</h2>
+      <h3 className="hero-greeting-mission-statement">I design and code beautiful websites</h3>
     </div>
   );
 };
