ea983ebaf07b8b214d394aeaa5ca9bb2a6037a5b
Its-A-Mia <miapurdun@gmail.com>
2023-06-15 22:45:05 -0500

    made the illustration the call to action

--- src/components/hero/HeroGreeting.jsx
+++ src/components/hero/HeroGreeting.jsx
@@ -4,9 +4,6 @@ const HeroGreeting = () => {
       <h1 className="hero-greeting-title">Hi, I'm Mia</h1>
       <h2 className="hero-greeting-subtitle">Budding software developer</h2>
       <h3 className="hero-greeting-mission-statement">I turn digital dreams into reality</h3>
-      {/* <a className="hero-call-to-action anchor-button" href="#projects">
-        PORTFOLIO
-      </a> */}
     </div>
   );
 };
--- src/components/hero/HeroIllustration.jsx
+++ src/components/hero/HeroIllustration.jsx
@@ -4,15 +4,46 @@ import HTMLWebIcon from '../icons/HTMLWebIcon';
 
 const HeroIllustration = () => {
   return (
-    <div className="hero-illustration-container">
+    <a className="hero-illustration-container" href="./#projects">
       <span className="hero-illustration-flower-one">
         <FlowerVariantTwo />
       </span>
       <HTMLWebIcon />
+
+      <div className="hero-illustration-text-container">
+        <p className="hero-illustration-text" id="hero-illustration-text-1">
+          {'<'}CHECK{'>'}
+          <span className="hero-illustration-text-highligher">
+            {'<'}CHECK{'>'}
+          </span>
+        </p>
+
+        <p className="hero-illustration-text" id="hero-illustration-text-2">
+          {'<'}OUT{'>'}
+          <span className="hero-illustration-text-highligher">
+            {'<'}OUT{'>'}
+          </span>
+        </p>
+
+        <p className="hero-illustration-text" id="hero-illustration-text-3">
+          {'<'}MY{'>'}
+          <span className="hero-illustration-text-highligher">
+            {'<'}MY{'>'}
+          </span>
+        </p>
+
+        <p className="hero-illustration-text" id="hero-illustration-text-4">
+          {'<'}PROJECTS{'>'}
+          <span className="hero-illustration-text-highligher">
+            {'<'}PROJECTS{'>'}
+          </span>
+        </p>
+      </div>
+
       <span className="hero-illustration-flower-two">
         <FlowerVariantOne />
       </span>
-    </div>
+    </a>
   );
 };
 
